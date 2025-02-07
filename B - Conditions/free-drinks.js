4d626b9eabd00e6a364e1538411aa69254ce1563
Omar Faruk <omarfaruk.dev@gmail.com>
2025-02-07 23:50:23 +0600

    conditionals problem added

---
+++ B - Conditions/free-drinks.js
@@ -0,0 +1,16 @@
+/*
+Problem - 1
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/ 
+
+let burgerPrice = 510;
+let cokePrice = 30;
+
+if (burgerPrice > 500) {
+    console.log("You got a free coke");
+}else {
+    console.log("Coke Price is: 30tk")
+}
